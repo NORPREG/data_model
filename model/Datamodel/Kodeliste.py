ba8ee02bee457c62693b77abcff3196abd27876a
Helge Egil Seime Pettersen <helge.e.s.pettersen@gmail.com>
2025-05-21 14:31:04 +0200

    Nye datafelt, nå parser den

--- model/Datamodel/Kodeliste.py
+++ model/Datamodel/Kodeliste.py
@@ -30,7 +30,7 @@ class Registry(BaseModel):
 
 class Patient(BaseModel):
     """Pasientobjekt. Kan inneholde flere behandlingsforløp."""
-    
+
     patient_key: int = field_with_meta(title="Registernøkkel", description="Pseudonymisert nøkkel for pasienten i registeret. Angis automatisk for hver pasient. Foreslått format er 7-karakter heksadesimal (f.eks. a72bf40)")
     dt_added: datetime = field_with_meta(title="Dato lagt til", description="Dato for når pasienten ble lagt inn i registeret. Angis når enten DICOM-datasettet eller EPJ-datasettet ankommer.")
 
@@ -59,10 +59,11 @@ class PatientID(BaseModel):
 
     id: int = field_with_meta(title="Radindeks for pasient ID", description="Pseudonymisert nøkkel for pasienten i registeret.")
     fk_patient_key: int = field_with_meta(title="Registernøkkel", description="Pseudonymisert nøkkel for pasienten i registeret")
-    patient: Optional[Patient] = field_with_meta(title="", description="", default=None)
-    dt_added: datetime = field_with_meta(title="", description="")
-    fnr_aes: str = field_with_meta(title="", description="", encrypted=True)
-    fnr_type: str = field_with_meta(title="", description="")
+    # patient: Optional[Patient] = field_with_meta(title="", description="", default=None)
+    dt_added: datetime = field_with_meta(title="Dato lagt til", description="Dato for når denne pasient-ID-en ble lagt til i registeret")
+    fnr_aes: str = field_with_meta(title="Pasientidentifikasjon", description="Kan være av ulik `fnr_type`", encrypted=True)
+    fnr_type: Literal["F", "H", "D"] = field_with_meta(title="Type pasientidentifikasjon", 
+        description="Dersom pasienten har flere tilknyttet seg, er det det siste F-nummeret som er gjeldende.", values=["F", "H", "D"])
 
 
 class Address(BaseModel):
@@ -71,7 +72,7 @@ class Address(BaseModel):
     id: int = field_with_meta(title="Radindeks for adresse", description="Dannes automatisk ved opprettelse av ny adresse")
     dt_added: datetime = field_with_meta(title="Dato lagt til", description="Dato for når aktuell adresse ble lagt til")
     fk_patient_key: int = field_with_meta(title="Registernøkkel", description="Pseudonymisert nøkkel for pasienten i registeret")
-    patient: Optional[Patient] = field_with_meta(title="Tilknyttet pasient", description="")
+    # patient: Optional[Patient] = field_with_meta(title="Tilknyttet pasient", description="")
 
     zip_code_aes: str = field_with_meta(title="Postnummer", description="Postnummer da pasienten ble lagt inn", encrypted=True)
     bydel_aes: str = field_with_meta(title="Bydel", description="Aktuell for Oslo.", encrypted=True)
@@ -84,48 +85,48 @@ class Course(BaseModel):
     id: int = field_with_meta(title="Radindeks for behandlingsforløp", description="Dannes automatisk ved opprettelse av nytt behandlingsforløp")
     dt_added: datetime = field_with_meta(title="Dato lagt til", description="Dato for når aktuelt behandlingsforløp ble lagt til")
     fk_patient_key: int = field_with_meta(title="Registernøkkel", description="Pseudonymisert nøkkel for pasienten i registeret")
-    patient: Optional[Patient] = field_with_meta(title="", description="", default=None)
+    patient: Optional[Patient] = field_with_meta(title="Tilknyttet pasient", description="Pasientobjekt som hører til dette behandlingsforløpet", default=None)
     patient_exports: List['PatientExport'] = field_with_meta(title="", description="", default_factory=list)
-    fk_datastatus_id: int = field_with_meta(title="", description="")
+    fk_datastatus_id: int = field_with_meta(title="FK datastatus ID", description="Koblingsnøkkel mot datastatus for dette behandlingsforløpet")
     data_status: Optional['DataStatus'] = field_with_meta(title="", description="", default=None)
-    ois_course_id: str = field_with_meta(title="", description="")
-    epj_course_id: str = field_with_meta(title="", description="")
+    ois_course_id_aes: str = field_with_meta(title="OIS course ID", description="Koblingsnøkkel for behandlingsforløp / course i stråleterapisystem", encrypted=True)
+    epj_course_id_aes: str = field_with_meta(title="EPJ course ID", description="Koblingsnøkkel for behandlingsforløp / sak i journalsystem", encrypted=True)
 
 
 class DataStatus(BaseModel):
     id: int = field_with_meta(title="Registernøkkel", description="Pseudonymisert nøkkel for pasienten i registeret.")
-    fk_course_id: int = field_with_meta(title="", description="")
+    fk_course_id: int = field_with_meta(title="FK course ID", description="Koblingsnøkkel for behandlingsforløp")
     course: Optional[Course] = field_with_meta(title="", description="", default=None)
-    epj_status_aes: int = field_with_meta(title="", description="", encrypted=True)
-    dicom_status_aes: int = field_with_meta(title="", description="", encrypted=True)
-    consent_status_aes: int = field_with_meta(title="", description="", encrypted=True)
-    prom_status_aes: int = field_with_meta(title="", description="", encrypted=True)
+    epj_status_aes: int = field_with_meta(title="EPJ status", description="Statuskode for dataoverføring EPJ. Har behov for en god datamodell eller støttetabell her, f.eks. for å logge hver hendelse med statusmeldinger", encrypted=True)
+    dicom_status_aes: int = field_with_meta(title="DICOM status", description="Statuskode for dataoverføring DICOM. Har behov for en god datamodell eller støttetabell her, f.eks. for å logge hver hendelse med statusmeldinger", encrypted=True)
+    consent_status_aes: int = field_with_meta(title="PVK status", description="Statuskode for personvernkomponenten. Tabellene PvkSync og PvkEvents inneholder mer informasjon om mottate meldinger her", encrypted=True)
+    prom_status_aes: int = field_with_meta(title="PROMs status", description="Statuskode for pasientrapporterte data. Har behov for en god datamodell eller støttetabell her, f.eks. for å logge hver hendelse med statusmeldinger", encrypted=True)
 
 
 class Study(BaseModel):
     id: int = field_with_meta(title="Registernøkkel", description="Pseudonymisert nøkkel for pasienten i registeret.")
-    conquest_name: str = field_with_meta(title="", description="")
-    description_aes: str = field_with_meta(title="", description="", encrypted=True)
-    contact_person_aes: str = field_with_meta(title="", description="", encrypted=True)
-    institution_aes: str = field_with_meta(title="", description="", encrypted=True)
-    email_aes: str = field_with_meta(title="", description="", encrypted=True)
-    store_until: datetime = field_with_meta(title="", description="")
+    conquest_name: str = field_with_meta(title="Conquest PACS AES title", description="Navnet på Conquest-instansen som er knyttet til dette studiet dersom det finnes")
+    description_aes: str = field_with_meta(title="Studienavn", description="Navn på studien eller kvalitetsprosjektet", encrypted=True)
+    contact_person_aes: str = field_with_meta(title="Kontaktperson", description="Registerets kontaktperson for denne studien", encrypted=True)
+    institution_aes: str = field_with_meta(title="Institusjon", description="Hvor ledes studien fra", encrypted=True)
+    email_aes: str = field_with_meta(title="Epost-adresse", description="Epost-adresse hvor kontaktpersonen kan nås", encrypted=True)
+    store_until: datetime = field_with_meta(title="Datavarighet", description="Etter denne datoen slettes de ekstra dataene som er lagret for denne studien")
 
-    exports: List['Export'] = field_with_meta(title="", description="", default_factory=list)
+    exports: List['Export'] = field_with_meta(title="Datautleveringer", description="Hvilke datautleveringer som er knyttet mot denne studien", default_factory=list)
 
 
 class Export(BaseModel):
     id: int = field_with_meta(title="Registernøkkel", description="Pseudonymisert nøkkel for pasienten i registeret.")
-    fk_study_id: int = field_with_meta(title="", description="")
+    fk_study_id: int = field_with_meta(title="FK study ID", description="Koblingsnøkkel mot forskningsstudie / kvalitetsprosjekt for datautlevering")
     study: Optional[Study] = field_with_meta(title="", description="", default=None)
-    patient_exports: List['PatientExport'] = field_with_meta(title="", description="", default_factory=list)
-    registry_exports: List['RegistryExport'] = field_with_meta(title="", description="", default_factory=list)
-    export_date: datetime = field_with_meta(title="", description="")
-    contact_person_aes: str = field_with_meta(title="", description="", encrypted=True)
-    institution_aes: str = field_with_meta(title="", description="", encrypted=True)
-    email_aes: str = field_with_meta(title="", description="", encrypted=True)
-    mechanism: str = field_with_meta(title="", description="")
-    is_pseudo: bool = field_with_meta(title="", description="")
+    patient_exports: List['PatientExport'] = field_with_meta(title="Pasientutleveringer", description="Mange-til-mange liste for pasienteksporter med enkeltvise unike pseudonymiserte nøkler", default_factory=list)
+    registry_exports: List['RegistryExport'] = field_with_meta(title="Registerutleveringer", description="Mange-til-mange liste for koblinger mot enkeltvise registre i denne utleveringen", default_factory=list)
+    export_date: datetime = field_with_meta(title="Utleveringsdato", description="Dato denne utleveringen fant sted (ikke søknadsdato)")
+    contact_person_aes: str = field_with_meta(title="Kontaktperson", description="Kontaktperson for denne enkeltvise utleveringen", encrypted=True)
+    institution_aes: str = field_with_meta(title="Institusjon", description="Institusjon for denne enkeltvise utleveringen", encrypted=True)
+    email_aes: str = field_with_meta(title="Epost-adresse", description="Epost-adresse for kontaktpersonen", encrypted=True)
+    mechanism: str = field_with_meta(title="Utleveringsmekanisme", description="Hvilken utleveringsmekanisme som er benyttet", values=["DICOM", "Filsluse", "USB", "Annet"])
+    is_pseudo: bool = field_with_meta(title="Pseudonymiserte data", description="Er data pseudonymiserte?")
 
 
 class PatientExport(BaseModel):
