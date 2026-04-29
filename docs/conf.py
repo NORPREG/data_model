2510d41c4d479ebbec27d97d6f714629c5e0316c
Helge Egil Seime Pettersen <helge.e.s.pettersen@gmail.com>
2026-04-29 11:07:58 +0200

    Satt på full verbose

--- docs/conf.py
+++ docs/conf.py
@@ -46,22 +46,22 @@ html_static_path = ['_static']
 html_logo = 'static/norpreg_logo.png'
 
 
-autodoc_pydantic_model_show_json = False
-autodoc_pydantic_model_show_config_list = False
+autodoc_pydantic_model_show_json = True
+autodoc_pydantic_model_show_config_list = True
 autodoc_pydantic_model_show_field_summary = True
 autodoc_pydantic_model_show_field_list = True
 autodoc_pydantic_model_member_order = "bysource"
 autodoc_pydantic_field_doc_policy = "both"
 autodoc_pydantic_field_swap_name_and_alias = True
 autodoc_pydantic_field_show_alias = True
-autodoc_pydantic_field_list_validators = False
-autodoc_pydantic_model_hide_paramlist = True
-autodoc_pydantic_settings_hide_paramlist = True
-autodoc_pydantic_model_show_validator_summary = False
+autodoc_pydantic_field_list_validators = True
+autodoc_pydantic_model_hide_paramlist = False
+autodoc_pydantic_settings_hide_paramlist = False
+autodoc_pydantic_model_show_validator_summary = True
 
-autodoc_pydantic_model_show_config_summary = False
-autodoc_pydantic_model_show_config_members = False
-autodoc_pydantic_model_show_validator_members = False
+autodoc_pydantic_model_show_config_summary = True
+autodoc_pydantic_model_show_config_members = True
+autodoc_pydantic_model_show_validator_members = True
 autodoc_pydantic_model_hide_reused_validator = True
 autodoc_pydantic_field_show_required = True
 autodoc_pydantic_field_show_default = True
